1763fc9785f32f247334f339058345f3a924294b
medovmisha <medovmikhail03@mail.ru>
2025-05-04 21:05:27 +0300

    add delete and find by code methods

--- src/main/java/com/example/CurrencyExchange/repositories/CurrencyRepository.java
+++ src/main/java/com/example/CurrencyExchange/repositories/CurrencyRepository.java
@@ -7,4 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CurrencyRepository extends JpaRepository<Currency, Long> {
 
+    Currency findByCode(String code);
+    void deleteByCode(String code);
 }
