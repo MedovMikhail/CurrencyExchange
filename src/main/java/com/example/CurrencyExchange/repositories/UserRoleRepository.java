4a1fb98d204744c4978cf89c6876fa92b042ce20
medovmisha <medovmikhail03@mail.ru>
2025-05-04 21:05:37 +0300

    add delete by role method

--- src/main/java/com/example/CurrencyExchange/repositories/UserRoleRepository.java
+++ src/main/java/com/example/CurrencyExchange/repositories/UserRoleRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRoleRepository extends JpaRepository<UserRole, Long> {
-
+    void deleteByRole(String role);
 }
