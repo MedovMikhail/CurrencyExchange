1a91f8c8bb334a989e3d85b0a40c7b7e1ec7dedc
medovmisha <medovmikhail03@mail.ru>
2025-05-04 13:18:41 +0300

    initial commit

---
+++ src/main/java/com/example/CurrencyExchenge/CurrencyExchengeApplication.java
@@ -0,0 +1,13 @@
+package com.example.CurrencyExchenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyExchengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyExchengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CurrencyExchenge/CurrencyExchengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CurrencyExchenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
