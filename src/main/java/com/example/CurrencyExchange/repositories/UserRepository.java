6554447f904d2468b020d360f6bc0b2be7a930db
medovmisha <medovmikhail03@mail.ru>
2025-05-06 20:18:16 +0300

    add find methods to UserRepository

--- src/main/java/com/example/CurrencyExchange/repositories/UserRepository.java
+++ src/main/java/com/example/CurrencyExchange/repositories/UserRepository.java
@@ -4,7 +4,11 @@ import com.example.CurrencyExchange.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    Optional<User> findByEmail(String email);
+    Optional<User> findByPhone(String phone);
+    Optional<User> findByName (String name);
 }
