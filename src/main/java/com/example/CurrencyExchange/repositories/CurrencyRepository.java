7c02067ee0e3792451cdf3de78cb98de5e4f5a5c
medovmisha <medovmikhail03@mail.ru>
2025-05-06 20:15:01 +0300

    add transactional for delete methods

--- src/main/java/com/example/CurrencyExchange/repositories/CurrencyRepository.java
+++ src/main/java/com/example/CurrencyExchange/repositories/CurrencyRepository.java
@@ -1,6 +1,7 @@
 package com.example.CurrencyExchange.repositories;
 
 import com.example.CurrencyExchange.entities.Currency;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +9,6 @@ import org.springframework.stereotype.Repository;
 public interface CurrencyRepository extends JpaRepository<Currency, Long> {
 
     Currency findByCode(String code);
+    @Transactional
     void deleteByCode(String code);
 }
--- src/main/java/com/example/CurrencyExchange/repositories/UserRoleRepository.java
+++ src/main/java/com/example/CurrencyExchange/repositories/UserRoleRepository.java
@@ -1,10 +1,14 @@
 package com.example.CurrencyExchange.repositories;
 
 import com.example.CurrencyExchange.entities.UserRole;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRoleRepository extends JpaRepository<UserRole, Long> {
+
+    UserRole findByRole(String role);
+    @Transactional
     void deleteByRole(String role);
 }
