7b861537487e0974466c9b2b910895ed323f2685
medovmisha <medovmikhail03@mail.ru>
2025-05-10 19:46:37 +0300

    add mapper from exchangedCurrency to exchangeCurrency

--- src/main/java/com/example/CurrencyExchange/utils/mapping/ExchangeCurrencyMapper.java
+++ src/main/java/com/example/CurrencyExchange/utils/mapping/ExchangeCurrencyMapper.java
@@ -1,6 +1,7 @@
 package com.example.CurrencyExchange.utils.mapping;
 
 import com.example.CurrencyExchange.dto.ExchangeCurrencyDTO;
+import com.example.CurrencyExchange.dto.kafka.ExchangedCurrencyDTO;
 import com.example.CurrencyExchange.entities.CashRegister;
 import com.example.CurrencyExchange.entities.Currency;
 import com.example.CurrencyExchange.entities.ExchangeCurrency;
@@ -41,6 +42,29 @@ public class ExchangeCurrencyMapper {
         to.setTargetCurrencyCode(from.getTargetCurrency().getCode());
         to.setCashRegisterId(from.getCashRegister().getId());
         to.setUserId(from.getUser().getId());
+        return to;
+    }
+
+    public ExchangeCurrencyDTO fromEntityToDTO(ExchangeCurrency from, String baseCurrencyCode, String targetCurrencyCode) {
+        if (from == null) return null;
+        ExchangeCurrencyDTO to = new ExchangeCurrencyDTO();
+        to.setId(from.getId());
+        to.setExchangeRate(from.getExchangeRate());
+        to.setCountBaseCash(from.getCountBaseCash());
+        to.setCountTargetCash(from.getCountTargetCash());
+        to.setDateOfExchange(from.getDateOfExchange());
+        to.setBaseCurrencyCode(baseCurrencyCode);
+        to.setTargetCurrencyCode(targetCurrencyCode);
+        to.setCashRegisterId(from.getCashRegister().getId());
+        to.setUserId(from.getUser().getId());
+        return to;
+    }
+
+    public ExchangeCurrencyDTO fromExchangedToExchange(ExchangeCurrencyDTO to, ExchangedCurrencyDTO from) {
+        to.setCountTargetCash(
+                from.getTargetStoredCurrencyDiff()
+        );
+        to.setDateOfExchange(from.getDateOfExchange());
         return to;
     }
 }
