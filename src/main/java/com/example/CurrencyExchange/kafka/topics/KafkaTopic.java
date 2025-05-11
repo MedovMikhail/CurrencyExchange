6d73b6df757a0c3ae173213cac649250fe6f195a
medovmisha <medovmikhail03@mail.ru>
2025-05-11 17:21:12 +0300

    add many topics

--- src/main/java/com/example/CurrencyExchange/kafka/topics/KafkaTopic.java
+++ src/main/java/com/example/CurrencyExchange/kafka/topics/KafkaTopic.java
@@ -12,10 +12,22 @@ public class KafkaTopic {
     private String sendToCurrenciesRate;
     @Value("${kafka.topic.waitCurrenciesRate}")
     private String waitCurrenciesRate;
+    @Value("${kafka.topic.sendToCurrenciesScale}")
+    private String sendToCurrenciesScale;
+    @Value("${kafka.topic.waitCurrenciesScale}")
+    private String waitCurrenciesScale;
     @Value("${kafka.topic.sendToExchangeCurrencies}")
     private String sendToExchangeCurrencies;
     @Value("${kafka.topic.waitExchangeCurrencies}")
     private String waitExchangeCurrencies;
+    @Value("${kafka.topic.sendToCurrencyRates}")
+    private String sendToCurrencyRates;
+    @Value("${kafka.topic.waitCurrencyRates}")
+    private String waitCurrencyRates;
+    @Value("${kafka.topic.sendToRecountCurrency}")
+    private String sendToRecountCurrency;
+    @Value("${kafka.topic.waitRecountCurrency}")
+    private String waitRecountCurrency;
 
     @Bean
     public NewTopic topic1() {
@@ -33,7 +45,32 @@ public class KafkaTopic {
     }
 
     @Bean
-    public NewTopic topic4() {
-        return TopicBuilder.name(waitExchangeCurrencies).build();
+    public NewTopic topic5() {
+        return TopicBuilder.name(sendToCurrenciesScale).build();
+    }
+
+    @Bean
+    public NewTopic topic6() {
+        return TopicBuilder.name(waitCurrenciesScale).build();
+    }
+
+    @Bean
+    public NewTopic topic7() {
+        return TopicBuilder.name(sendToCurrencyRates).build();
+    }
+
+    @Bean
+    public NewTopic topic8() {
+        return TopicBuilder.name(waitCurrencyRates).build();
+    }
+
+    @Bean
+    public NewTopic topic9() {
+        return TopicBuilder.name(sendToRecountCurrency).build();
+    }
+
+    @Bean
+    public NewTopic topic10() {
+        return TopicBuilder.name(waitRecountCurrency).build();
     }
 }
