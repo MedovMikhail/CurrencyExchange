f7dd17ce1a098de5a5b13282e15b47c22d3e8e23
medovmisha <medovmikhail03@mail.ru>
2025-05-11 14:49:56 +0300

    rework fields

--- src/main/java/com/example/CurrencyExchange/dto/kafka/CurrencyRecountDTO.java
+++ src/main/java/com/example/CurrencyExchange/dto/kafka/CurrencyRecountDTO.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.List;
 
 @Data
@@ -12,6 +14,6 @@ import java.util.List;
 @NoArgsConstructor
 public class CurrencyRecountDTO {
 
-    private StoredCurrencyDTO base;
-    private List<StoredCurrencyDTO> others;
+    private HashMap<String, BigDecimal> currencyRates;
+    private List<StoredCurrencyDTO> storedCurrencies;
 }
